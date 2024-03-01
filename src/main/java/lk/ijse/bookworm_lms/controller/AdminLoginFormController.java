d5cf80206d8e3e6537ff46c42a9761b7ffca2745
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-03-01 19:32:34 +0530

    admin login pane created...

---
+++ src/main/java/lk/ijse/bookworm_lms/controller/AdminLoginFormController.java
@@ -0,0 +1,18 @@
+package lk.ijse.bookworm_lms.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AdminLoginFormController {
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private void btnLoginOnAction() {
+
+    }
+}
--- src/main/java/lk/ijse/bookworm_lms/controller/HomeFormController.java
+++ src/main/java/lk/ijse/bookworm_lms/controller/HomeFormController.java
@@ -1,14 +1,38 @@
 package lk.ijse.bookworm_lms.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class HomeFormController {
 
     @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
     private void btnAdminOnAction() {
+        pane.getChildren().clear();
+        try {
+            AnchorPane anchorPane = FXMLLoader.load(this.getClass().getResource("/view/adminLogin_form.fxml"));
+            pane.getChildren().add(anchorPane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     private void btnUserOnAction() {
+        pane.getChildren().clear();
+        try {
+            AnchorPane anchorPane = FXMLLoader.load(this.getClass().getResource("/view/PropertyForm.fxml"));
+            pane.getChildren().add(anchorPane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
