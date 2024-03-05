e0995602fb68310d19e71abbe49a3693fedc0935
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-03-05 12:26:04 +0530

    admin dashboard created...

--- src/main/java/lk/ijse/bookworm_lms/controller/AdminDashboardFormController.java
+++ src/main/java/lk/ijse/bookworm_lms/controller/AdminDashboardFormController.java
@@ -1,4 +1,37 @@
 package lk.ijse.bookworm_lms.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class AdminDashboardFormController {
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtMail;
+
+    @FXML
+    private TextField txtManager;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    private void btnDeleteOnAction() {
+
+    }
+
+    @FXML
+    private void btnSaveOnAction() {
+
+    }
+
+    @FXML
+    private void btnUpdateOnAction() {
+
+    }
 }
