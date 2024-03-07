378f1608694198a5935a30dd37a6c5b335a85ea2
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-03-07 11:15:33 +0530

    Book manage ui created...

---
+++ src/main/java/lk/ijse/bookworm_lms/controller/BranchFormController.java
@@ -0,0 +1,81 @@
+package lk.ijse.bookworm_lms.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class BranchFormController {
+
+    @FXML
+    private TableColumn<?, ?> colAuther;
+
+    @FXML
+    private TableColumn<?, ?> colBookId;
+
+    @FXML
+    private TableColumn<?, ?> colBranchId;
+
+    @FXML
+    private TableColumn<?, ?> colGenre;
+
+    @FXML
+    private TableColumn<?, ?> colStatus;
+
+    @FXML
+    private TableColumn<?, ?> colTimeStamp;
+
+    @FXML
+    private TableColumn<?, ?> colTitle;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private TableView<?> tblCustomer;
+
+    @FXML
+    private TextField txtAuther;
+
+    @FXML
+    private TextField txtBranch;
+
+    @FXML
+    private TextField txtGenre;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    private TextField txtTitle;
+
+    @FXML
+    private void btnClearOnAction() {
+
+    }
+
+    @FXML
+    private void btnDeleteOnAction() {
+
+    }
+
+    @FXML
+    private void btnSaveOnAction() {
+
+    }
+
+    @FXML
+    private void btnSearchOnAction() {
+
+    }
+
+    @FXML
+    private void btnUpdateOnAction() {
+
+    }
+
+}
